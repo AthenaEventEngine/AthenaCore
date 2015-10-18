2e8dbc7b1143109d2f5e49fefe92913fc5b7a487
U3Games <swarlog@hotmail.com>
2015-10-19 01:12:55 +0200

    Updated code..

--- java/net/sf/eventengine/task/AntiAfkTask.java
+++ java/net/sf/eventengine/task/AntiAfkTask.java
@@ -19,13 +19,19 @@
 
 package net.sf.eventengine.task;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.l2jserver.gameserver.instancemanager.InstanceManager;
+import com.l2jserver.gameserver.model.Location;
+import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
+import com.l2jserver.gameserver.model.instancezone.InstanceWorld;
 
 import net.sf.eventengine.EventEngineManager;
 import net.sf.eventengine.enums.EventEngineState;
+import net.sf.eventengine.events.handler.AbstractEvent;
 import net.sf.eventengine.events.holders.PlayerHolder;
+import net.sf.eventengine.util.ProtectionUtil;
 
 /**
  * @author swarlog
@@ -33,7 +39,11 @@ import net.sf.eventengine.events.holders.PlayerHolder;
 
 public class AntiAfkTask implements Runnable
 {
-	private final Map<Integer, List<PlayerHolder>> _players = new HashMap<>();
+	// Debug
+	static boolean debug = false;
+	
+	// Player List
+	private final Set<L2PcInstance> _playerAfkInstance = ConcurrentHashMap.newKeySet();
 	
 	@Override
 	public void run()
@@ -43,7 +53,28 @@ public class AntiAfkTask implements Runnable
 		{
 			case RUNNING_EVENT:
 			{
-				// TODO: Save, check position and effect player.
+				for (PlayerHolder ph : EventEngineManager.getInstance().getCurrentEvent().getAllEventPlayers())
+				{
+					if (ph == null)
+					{
+						continue;
+					}
+					
+					if (ph.getPcInstance().isOnline())
+					{
+						// Check player list
+						if (_playerAfkInstance.contains(ph))
+						{
+							// Execute check location
+							playerCheckLocation(ph);
+						}
+						else
+						{
+							// Execute save location
+							playerSaveLocation(ph);
+						}
+					}
+				}
 				break;
 			}
 		}
@@ -51,7 +82,74 @@ public class AntiAfkTask implements Runnable
 		// Clear list players
 		if (state != EventEngineState.RUNNING_EVENT)
 		{
-			_players.clear();
+			_playerAfkInstance.clear();
+			ProtectionUtil._playersAfkLoc.clear();
+		}
+	}
+	
+	public void playerSaveLocation(PlayerHolder ph)
+	{
+		if (ph != null)
+		{
+			// Save Player in list
+			_playerAfkInstance.add(ph.getPcInstance());
+			
+			// Save Location
+			ProtectionUtil.addAfkLoc(ph);
+			
+			// Test I
+			if (debug)
+			{
+				System.out.println("[ANTI-AFK] Location Test I - Player: " + ph.getPcInstance().getName() + ", locX: " + ph.getPcInstance().getX() + ", locY: " + ph.getPcInstance().getY() + ", locZ: " + ph.getPcInstance().getZ());
+			}
+		}
+	}
+	
+	public void playerCheckLocation(PlayerHolder ph)
+	{
+		if (ph != null)
+		{
+			// Check Location
+			Location phLoc = ProtectionUtil.getAfkLocation(ph);
+			if (phLoc != null)
+			{
+				if (phLoc.equals(ph.getPcInstance().getLocation()))
+				{
+					AbstractEvent currentEvent = EventEngineManager.getInstance().getCurrentEvent();
+					
+					// Test II
+					if (debug)
+					{
+						System.out.println("[ANTI-AFK] Location Test II - Player: " + ph.getPcInstance().getName() + ", locX: " + ph.getPcInstance().getX() + ", locY: " + ph.getPcInstance().getY() + ", locZ: " + ph.getPcInstance().getZ());
+					}
+					
+					// Remove all effect of player
+					ph.getPcInstance().stopAllEffects();
+					
+					// Recover the title and color of the participants.
+					ph.recoverOriginalColorTitle();
+					ph.recoverOriginalTitle();
+					
+					// It out of the world created for the event
+					InstanceWorld world = InstanceManager.getInstance().getPlayerWorld(ph.getPcInstance());
+					world.removeAllowed(ph.getPcInstance().getObjectId());
+					ph.getPcInstance().setInstanceId(0);
+					
+					// Dead Player
+					currentEvent.revivePlayer(ph);
+					
+					// Teleport Player
+					// FIXME We send a character to their actual instance and turn
+					ph.getPcInstance().teleToLocation(83437, 148634, -3403, 0, 0);// GIRAN CENTER
+					
+					// Remove player from event
+					currentEvent.getAllEventPlayers().remove(ph);
+					ProtectionUtil.removeAfkLoc(ph);
+					
+					_playerAfkInstance.clear();
+					ProtectionUtil._playersAfkLoc.clear();
+				}
+			}
 		}
 	}
 }
