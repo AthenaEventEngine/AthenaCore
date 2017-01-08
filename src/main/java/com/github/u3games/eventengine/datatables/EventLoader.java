68994215fd888408891354bd65478f786bf10abc
Lucas <luksdlt92@gmail.com>
2017-01-08 11:30:14 -0300

    Cleanup jar loader and files creation

--- src/main/java/com/github/u3games/eventengine/datatables/EventLoader.java
+++ src/main/java/com/github/u3games/eventengine/datatables/EventLoader.java
@@ -3,24 +3,20 @@ package com.github.u3games.eventengine.datatables;
 import com.github.u3games.eventengine.config.model.MainEventConfig;
 import com.github.u3games.eventengine.interfaces.EventContainer;
 import com.github.u3games.eventengine.util.GsonUtils;
+import com.github.u3games.eventengine.util.JarUtils;
 import com.l2jserver.util.Rnd;
 
-import java.io.*;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.*;
-import java.util.jar.Attributes;
-import java.util.jar.JarFile;
-import java.util.jar.Manifest;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EventLoader {
 
-    private static final Logger LOGGER = Logger.getLogger(EventLoader.class.getName());
     private static final String MAIN_CONFIG_PATH = "./eventengine/EventEngine.conf";
     private static final String EVENT_JAR_PATH = "./eventengine/events/";
+    private static final String EVENT_CONFIG_NAME = "config.conf";
 
     private final ArrayList<EventContainer> _eventList = new ArrayList<>();
     private final Map<String, EventContainer> _eventMap = new HashMap<>();
@@ -59,98 +55,18 @@ public class EventLoader {
 
         if (matchingFiles != null) {
             for (File jar : matchingFiles) {
-                EventContainer container = (EventContainer) loadJar((jar), getMainClass(jar));
+                EventContainer container = (EventContainer) JarUtils.loadJar(getClass().getClassLoader(), jar);
 
-                if (container != null && container.checkStructure())
+                if (container != null)
                 {
-                    _eventList.add(container);
-                    _eventMap.put(container.getSimpleEventName(), container);
+                    String folderPath = EVENT_JAR_PATH + container.getSimpleEventName() + "/";
+
+                    if (JarUtils.writeFileFromResources(container.getClass().getClassLoader(), folderPath, EVENT_CONFIG_NAME)) {
+                        _eventList.add(container);
+                        _eventMap.put(container.getSimpleEventName(), container);
+                    }
                 }
             }
-        }
-    }
-
-    private String getMainClass(File file)
-    {
-        try {
-            // Open the JAR file
-            JarFile jarfile = new JarFile(file);
-
-            // Get the manifest
-            Manifest manifest = jarfile.getManifest();
-
-            // Get the main attributes in the manifest
-            Attributes attrs = manifest.getMainAttributes();
-
-            // Enumerate each attribute
-            for (Iterator it=attrs.keySet().iterator(); it.hasNext(); ) {
-                // Get attribute name
-                Attributes.Name attrName = (Attributes.Name)it.next();
-
-                if (attrName.toString().equalsIgnoreCase("Main-Class")) {
-                    return attrs.getValue(attrName);
-                }
-            }
-        }
-        catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-
-        return null;
-    }
-
-    private Object loadJar(File jar, String classPath)
-    {
-        try {
-            URL classUrl;
-            URL[] classUrls;
-
-            try {
-                classUrl = jar.toURI().toURL();
-                classUrls = new URL[] { classUrl };
-            } catch (MalformedURLException ex) {
-                throw new InvalidJarLoadException("File URL malformed " + jar.getName());
-            }
-
-            URLClassLoader child = new URLClassLoader(classUrls, this.getClass().getClassLoader());
-            Class classToLoad;
-
-            try {
-                classToLoad = Class.forName(classPath, true, child);
-            } catch (ClassNotFoundException ex) {
-                throw new InvalidJarLoadException("Cannot find main class " + classPath);
-            }
-
-            if (!classToLoad.getSuperclass().getSimpleName().equalsIgnoreCase("BaseEventContainer")) {
-                throw new InvalidJarLoadException("Wrong inheritance for " + classToLoad.getSimpleName());
-            }
-
-            try {
-                return classToLoad.newInstance();
-            } catch (InstantiationException ex) {
-                throw new InvalidJarLoadException("The class " + classToLoad.getSimpleName() + " cannot be instantiated");
-            } catch (IllegalAccessException ex) {
-                throw new InvalidJarLoadException("The class " + classToLoad.getSimpleName() + " cannot be accessed");
-            }
-        } catch (InvalidJarLoadException ex) {
-            LOGGER.log(Level.SEVERE, ex.getMessage());
-        }
-
-        return null;
-    }
-
-    private class InvalidJarLoadException extends Exception {
-
-        private final String mMessage;
-
-        private InvalidJarLoadException(String message) {
-            super();
-            mMessage = message;
-        }
-
-        @Override
-        public final String getMessage() {
-            return InvalidJarLoadException.class.getSimpleName() + mMessage;
         }
     }
 
--- src/main/java/com/github/u3games/eventengine/model/BaseEventContainer.java
+++ src/main/java/com/github/u3games/eventengine/model/BaseEventContainer.java
@@ -5,7 +5,6 @@ import com.github.u3games.eventengine.events.handler.AbstractEvent;
 import com.github.u3games.eventengine.interfaces.EventContainer;
 import com.github.u3games.eventengine.util.GsonUtils;
 
-import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -34,91 +33,6 @@ public abstract class BaseEventContainer implements EventContainer {
         return _config;
     }
 
-    @Override
-    public boolean checkStructure() {
-        File folder = new File("./eventengine/events/" + getSimpleEventName());
-
-        if (!folder.exists() && !folder.mkdir()) {
-            return false;
-        } else {
-            ClassLoader classLoader = getClass().getClassLoader();
-
-            System.out.println("Blabla2 " + classLoader.getResource("config.conf"));
-            InputStream is = classLoader.getResourceAsStream("config.conf");
-
-            //System.out.println("Blabla3 " + getStringFromInputStream(is));
-            try {
-                writeFile(is);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            /*File file = new File(classLoader.getResource("/config.conf").getFile());
-
-            StringBuilder result = new StringBuilder("");
-
-            try (Scanner scanner = new Scanner(file)) {
-
-                while (scanner.hasNextLine()) {
-                    String line = scanner.nextLine();
-                    result.append(line).append("\n");
-                }
-
-                scanner.close();
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }*/
-        }
-
-        return true;
-    }
-
-    private String getStringFromInputStream(InputStream is) {
-
-        BufferedReader br = null;
-        StringBuilder sb = new StringBuilder();
-
-        String line;
-        try {
-
-            br = new BufferedReader(new InputStreamReader(is));
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        return sb.toString();
-
-    }
-
-    private void writeFile(InputStream is) throws IOException {
-        OutputStream os = new FileOutputStream("./eventengine/events/" + getSimpleEventName() + "/config.conf");
-
-        byte[] buffer = new byte[is.available()];
-
-        int bytesRead;
-        //read from is to buffer
-        while((bytesRead = is.read(buffer)) !=-1){
-            os.write(buffer, 0, bytesRead);
-        }
-        is.close();
-        //flush OutputStream to write any buffered data to file
-        os.flush();
-        os.close();
-    }
-
     public AbstractEvent newEventInstance()
     {
         EventBuilder builder = new EventBuilder();
@@ -127,7 +41,7 @@ public abstract class BaseEventContainer implements EventContainer {
         return builder.build();
     }
 
-    private class EventBuilder {
+    private static class EventBuilder {
 
         private final Logger LOGGER = Logger.getLogger(EventBuilder.class.getName());
 
